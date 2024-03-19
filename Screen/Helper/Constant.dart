f7131aef3a4be326d17d4fe9f8678fb6ac5bdab1
anjalialphawizz <152967662+anjalialphawizz@users.noreply.github.com>
2024-03-19 19:50:00 +0530

    Add files via upload

---
+++ Screen/Helper/Constant.dart
@@ -0,0 +1,31 @@
+final String appName = 'KRF';
+
+final String packageName = 'com.KRF_user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.KRF_user';
+final String iosLink = 'your ios link here';
+final String appStoreId = '123456789';
+
+final String deepLinkUrlPrefix = 'https://eshopmultivendor.page.link';
+final String deepLinkName = 'KRF';
+
+final int timeOut = 50;
+const int perPage = 1000;
+
+//final String baseUrl = 'https://vendor.eshopweb.store/app/v1/api/';
+// final String baseUrl = 'https://alphawizztest.tk/Eatoz Food_Ecom/app/v1/api/';
+// final String baseUrl = 'https://foodontheways.com/app/v1/api/';
+//final String baseUrl = 'https://foodontheways.com/New_food/app/v1/api/';
+/*final String baseUrl =
+    'https://eatoz.in/app/v1/api/';*/
+//final String baseUrl = "https://eatoz.in/app/v1/api/";
+//  final String baseUrl = "https://developmentalphawizz.com/zipcart/app/v1/api/";
+//final String baseUrl = "https://developmentalphawizz.com/eatop/app/v1/api/";
+final String baseUrl = "https://developmentalphawizz.com/KRF/app/v1/api/";
+
+final String imageUrl = 'https://developmentalphawizz.com/';
+// final String imageUrl = 'https://foodontheways.com/';
+final String jwtKey = "0352e7a815f965e8e278d47976b8f9cf466e1f42";
+double latitudeFirst = 0;
+double longitudeFirst = 0;
